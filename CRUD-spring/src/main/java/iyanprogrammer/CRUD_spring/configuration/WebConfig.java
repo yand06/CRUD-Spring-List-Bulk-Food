0d1b50807e199edb6a5cccb4ea338c7ceb77f100
Supriyandi La Awe <108932546+yand06@users.noreply.github.com>
2025-02-15 20:44:18 +0700

    Update WebConfig.java

--- CRUD-spring/src/main/java/iyanprogrammer/CRUD_spring/configuration/WebConfig.java
+++ CRUD-spring/src/main/java/iyanprogrammer/CRUD_spring/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package iyanprogrammer.CRUD_spring.configuration;
 
-// Mengimpor kelas yang diperlukan untuk konfigurasi MVC di Spring.
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
