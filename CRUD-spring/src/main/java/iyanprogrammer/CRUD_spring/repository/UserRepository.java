a9a7f55c1fa783d2bad488cc405f5b18907e5485
Supriyandi La Awe <108932546+yand06@users.noreply.github.com>
2025-02-15 20:45:46 +0700

    Update UserRepository.java

--- CRUD-spring/src/main/java/iyanprogrammer/CRUD_spring/repository/UserRepository.java
+++ CRUD-spring/src/main/java/iyanprogrammer/CRUD_spring/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package iyanprogrammer.CRUD_spring.repository;
 
-// Mengimpor kelas User dari model dan JpaRepository dari Spring Data JPA.
 import iyanprogrammer.CRUD_spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
